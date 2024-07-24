ad51fc576c919a6b65a5f1ff806d28eeefae0540
brunopc-net <work@brunopc.net>
2024-07-23 22:33:06 -0400

    Again

--- src/components/SkillTable/index.js
+++ src/components/SkillTable/index.js
@@ -4,7 +4,7 @@ import Link from '@docusaurus/Link';
 
 function getTagLink(tag){
     return "/docs/tags/"+tag
-        .replace(/c++/gi, 'c')
+        .replace(/c\+\+/gi, 'c')
         .replace(/unit/gi, '-unit')
         .replace(/([a-z])([A-Z])/g, '$1-$2')
         .replace(/([a-zA-Z])([0-9])/g, '$1-$2')
