06f0d9c04b428c0ca12d587592d70d7e0a7e0f0e
brunopc-net <work@brunopc.net>
2024-08-14 20:27:43 -0400

    Tag bug fix

--- src/components/SkillTable/index.js
+++ src/components/SkillTable/index.js
@@ -5,6 +5,7 @@ import Link from '@docusaurus/Link';
 
 function getTagLink(tag){
     return "/docs/tags/"+tag
+        .replace('C++', 'C')
         .replace(/unit/gi, '-unit')
         .replace(/([a-z])([A-Z])/g, '$1-$2')
         .replace(/([a-zA-Z])([0-9])/g, '$1-$2')
